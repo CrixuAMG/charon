1c83af779fa7860f9df5fd5f8316da5149f61c61
Christian Kaal <christiankaal@Mac-mini-van-Christian.local>
2025-12-27 23:08:34 +0100

    issue-4 Add shortcut to open a projects tasks

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/crixuamg/charon/internal/config"
+	"github.com/crixuamg/charon/internal/kitty"
 	"github.com/crixuamg/charon/internal/tui"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -19,6 +21,22 @@ func main() {
 
 	model := tui.NewModel(cfg)
 	defer model.Cleanup()
+
+	// If the user called charon with an argument, like `charon project` we will attempt to open that project or exit with an error
+	if (len(os.Args) == 2) {
+		project := os.Args[1]
+
+		projectCfg, err := cfg.FindProject(project)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := kitty.OpenProject(*projectCfg, cfg); err != nil {
+			log.Fatal(err)
+		}
+
+		os.Exit(0)
+	}
 
 	p := tea.NewProgram(model, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
